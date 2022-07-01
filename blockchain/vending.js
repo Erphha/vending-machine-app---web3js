da734c716d70b79fa64e61b8608186cd9a778e7f
Erfan Mougahi <Erphhan@gmail.com>
2022-07-01 23:30:43 +0430

    setting abi

--- blockchain/vending.js
+++ blockchain/vending.js
@@ -1,1 +1,10 @@
 import Web3 from 'web3'
+
+const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/79951bffb65a46e3a848434b74e03f78"
+)
+
+const web3 = new Web3(provider)
+
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
